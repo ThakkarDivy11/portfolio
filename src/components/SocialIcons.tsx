dad5ddeba8d03aaaf7ef338a1c5038660cb45b37
ThakkarDivy11 <divythakkar318@gmail.com>
2026-04-21 16:37:00 +0530

    image

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -80,7 +80,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#">
+      <a className="resume-button" href="https://drive.google.com/file/d/1DdEBZhO7W0hl_zl01sXehYTAFu-VMij3/view?usp=drive_link">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
